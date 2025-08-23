9a7804b1ca17ead60e3defd77504aa993bc1e977
chloe <kojlax@protonmail.com>
2025-08-22 22:48:20 -0400

    initial release

---
+++ src/commands.rs
@@ -0,0 +1,27 @@
+use crate::Data;
+use crate::dr_choicers::CHOICERS;
+
+use rand::Rng;
+
+type Context<'a> = poise::Context<'a, Data, anyhow::Error>;
+
+pub fn get_commands() -> Vec<poise::Command<Data, anyhow::Error>> {
+    vec![ask()]
+}
+
+/// Ask The Angel for some advice
+#[poise::command(slash_command, prefix_command, discard_spare_arguments)]
+// query is a placebo value and won't be used
+// I don't know if underscoring it will go through to discord so this is what I do
+#[allow(unused_variables)]
+pub async fn ask (
+    ctx: Context<'_>, 
+    #[rest] query: Option<String>
+) -> anyhow::Result<()> {
+    let max = CHOICERS.len();
+
+    let i = rand::rng().random_range(..max);
+
+    ctx.reply(CHOICERS[i]).await?;
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+mod commands;
+mod dr_choicers;
+
+use poise::serenity_prelude as serenity;
+
+pub fn data_dir() -> std::path::PathBuf {
+    let mut dir = dirs::home_dir().unwrap();
+    dir.push(".local");
+    dir.push("share");
+    dir.push("ata");
+    return dir;
+}
+
+pub struct Data {
+}
+
+impl Data {
+    pub fn new() -> Self {
+        Self {
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut path = data_dir();
+    path.push("pwd");
+    let token = std::fs::read_to_string(path).unwrap();
+    let intents = serenity::GatewayIntents::non_privileged().union(serenity::GatewayIntents::MESSAGE_CONTENT);
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: commands::get_commands(),
+            prefix_options: poise::structs::PrefixFrameworkOptions {
+                prefix: Some(";".into()),
+                additional_prefixes: vec![
+                    poise::Prefix::Literal("; "),
+                ],
+                mention_as_prefix: true,
+                ignore_bots: false,
+                ignore_thread_creation: true,
+                case_insensitive_commands: true,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data::new())
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    
+    client.unwrap().start().await.unwrap();
+}
