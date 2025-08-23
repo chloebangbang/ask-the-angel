cff065875da6f2a7936c7ac3cf625328d91b6c8f
chloe <kojlax@protonmail.com>
2025-08-23 17:29:45 -0400

    add embeds on slash commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,8 @@
 use crate::Data;
 use crate::dr_choicers::CHOICERS;
 
+use poise::serenity_prelude as serenity;
+
 use rand::Rng;
 
 type Context<'a> = poise::Context<'a, Data, anyhow::Error>;
@@ -16,9 +18,6 @@ pub fn get_commands() -> Vec<poise::Command<Data, anyhow::Error>> {
     interaction_context = "Guild|BotDm|PrivateChannel",
     prefix_command, discard_spare_arguments,
 )]
-// query is a placebo value and won't be used
-// I don't know if underscoring it will go through to discord so this is what I do
-#[allow(unused_variables)]
 pub async fn ask (
     ctx: Context<'_>, 
     #[rest] query: Option<String>
@@ -26,7 +25,31 @@ pub async fn ask (
     let max = CHOICERS.len();
 
     let i = rand::rng().random_range(..max);
+    
+    // if the command wasn't invoked with a slash command, don't include an embed
+    if !ctx.invocation_string().starts_with('/') {
+        ctx.reply(CHOICERS[i]).await?;
+    } else {
+        // only create an embed if there's query text
+        if let Some(query_content) = query {
+            // create the embed author
+            let mut embed_author = serenity::CreateEmbedAuthor::new(ctx.author().display_name());
+            if let Some(avatar) = ctx.author().avatar_url() {
+                embed_author = embed_author.icon_url(avatar);
+            }
 
-    ctx.reply(CHOICERS[i]).await?;
+            // create the embed
+            let embed = serenity::CreateEmbed::new()
+                .author(embed_author)
+                .description(query_content);
+
+            let reply = poise::CreateReply::default().content(CHOICERS[i]).embed(embed.clone());
+            ctx.send(reply).await?;
+        } else {
+            ctx.reply(CHOICERS[i]).await?;       
+        }
+    }
+    
+
     Ok(())
 }
